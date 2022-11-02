0103058d21454a7b685b0e05b5af3616102e354f
Javier Ortiz <javieroc@gmail.com>
2022-11-02 13:27:38 +0100

    feat(redirect) use environment variables

--- next.config.js
+++ next.config.js
@@ -1,7 +1,25 @@
 /** @type {import('next').NextConfig} */
+
+const URI = process.env.TO_URI || 'http://localhost:3000';
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  poweredByHeader: false,
+  redirects: async () => {
+    return [
+      {
+        source: '/:slug*',
+        destination: `${URI}/:slug*`,
+        permanent: true
+      },
+      {
+        source: '/',
+        destination: `${URI}/`,
+        permanent: true
+      }
+    ]
+  },
 }
 
 module.exports = nextConfig
