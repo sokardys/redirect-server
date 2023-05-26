4507b8f1f580d0fb1743b6900d794d3b003672a0
Javier Ortiz <javieroc@gmail.com>
2023-05-26 17:19:35 +0200

    feat(redirect) added declare ALL_TO_URI

--- next.config.js
+++ next.config.js
@@ -1,24 +1,43 @@
 /** @type {import('next').NextConfig} */
 
-const URI = process.env.TO_URI || 'http://localhost:3000';
+const TO_URI = process.env.TO_URI;
+const ALL_TO_URI= process.env.ALL_TO_URI;
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   poweredByHeader: false,
   redirects: async () => {
-    return [
-      {
-        source: '/:slug*',
-        destination: `${URI}/:slug*`,
-        permanent: true
-      },
-      {
-        source: '/',
-        destination: `${URI}/`,
-        permanent: true
-      }
-    ]
+    if (TO_URI) {
+      console.log('TO_URI');
+      return [
+        {
+          source: '/:slug*',
+          destination: `${TO_URI}/:slug*`,
+          permanent: true
+        },
+        {
+          source: '/',
+          destination: `${TO_URI}/`,
+          permanent: true
+        }
+      ]
+    }
+    if (ALL_TO_URI) {
+      console.log('ALL_TO_URI');
+      return [
+        {
+          source: '/:slug*',
+          destination: `${ALL_TO_URI}`,
+          permanent: true
+        },
+        {
+          source: '/',
+          destination: `${ALL_TO_URI}/`,
+          permanent: true
+        }
+      ]
+    }
   },
 }
 
